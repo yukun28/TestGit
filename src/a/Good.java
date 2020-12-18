8193634bb3d103b72b9ff9ec07c897d56ead8abf
yk <1349554138@qq.com>
2020-12-18 16:40:46 +0800

    刚练习一下

---
+++ src/a/Good.java
@@ -0,0 +1,12 @@
+package a;
+class A{
+    int w=3;
+    double f=6.7;
+    String fg="很好啊";
+}
+public class Good {
+    public static void main(String[] args) {
+        A c=new A();
+        System.out.println(c.fg);
+    }
+}
